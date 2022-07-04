ccc47f25185fc4ec9096fc2931cf5e91609aff67
aripin <enterprisearipin@gmail.com>
2022-07-04 14:07:20 +0700

    first commit

---
+++ hello/ifelse.py
@@ -0,0 +1,100 @@
+# cuaca = input("Cuaca sekarang :" )
+
+# if cuaca == "hujan" :
+#     print("Alfath Bawa Payung")
+# else : 
+#     print("Alfath G bawa payung")
+
+# ----------------------------------------
+
+
+# input_angka = int(input(("input angka : ")))
+
+# if input_angka % 2 == 1:
+#     print("Ini Angka ganjil")
+# else:
+#     print("Ini angka genap")
+
+# ----------------------------------------
+
+# inp = int(input("Masukkan angka : "))
+
+# if inp < 0:
+#     print("negatif")
+# elif inp > 0:
+#     print("positif")
+# else:
+#     print("nol")
+
+# ----------------------------------------
+
+# nilai = int(input("Masukkan Nilai : "))
+
+# if nilai >= 100:
+#     print("Invalid nilai")
+# elif nilai >= 90:
+#     print("A")
+# elif nilai >= 80:
+#     print("B")
+# elif nilai >= 70:
+#     print("C")
+# elif nilai >= 60:
+#     print("D")
+# else:
+#     print("E")
+
+    
+# ----------------------------------------
+# Persegi Panjang
+
+
+# from cgitb import text
+
+
+# p = int(input("Panjang : "))
+# l = int(input("Lebar : "))
+
+# luas = p*l
+# keliling = 2*p + 2*l
+
+# print("Luas : ", + luas)
+# print("keliling :", + keliling)
+
+# ----------------------------------------
+# persegi
+# s = int(input("S :"))
+
+# luas = s*s
+# keliling = 4*s
+
+# print("Luas : ", + luas)
+# print("Keliling : ", + keliling)
+
+# ----------------------------------------
+# lingkaran
+
+# j = int(input("jari-jari : "))
+# pi = 3.14
+
+# luas = pi*j**2
+# keliling = 2*pi*j
+
+# print("luas : ", + luas)
+# print("Keliling : ", + keliling)
+
+
+# --------------------
+
+# penghitung gaji 
+
+kerja = int(input("Jumlah Jam Kerja : "))
+lembur = int(input("Jumlah Lembur : "))
+absen = int(input("Jumlah Tidak Hadir : "))
+
+gaji = kerja * 15000 + lembur * 10000 - (absen * 100000)
+uangmakan =  kerja * 10000
+
+print("Gaji : ", + gaji)
+print ("Uang Makan :", + uangmakan )
+
+
